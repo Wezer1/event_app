d7534c05b0fbe70c67ce83562aeefe2da01e806d
Maksim <masyanya.maksimka@mail.ru>
2025-07-20 22:20:27 +0300

    Добавить https запросы

--- src/main/java/com/example/events_app/config/SecurityConfig.java
+++ src/main/java/com/example/events_app/config/SecurityConfig.java
@@ -55,6 +55,7 @@ public class SecurityConfig {
                 .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                 .requestMatchers(HttpMethod.POST,"/api/users/registration","/api/auth/login").permitAll()
                 .requestMatchers("/uploads/**").permitAll()
+                .requestMatchers("/api/ping").permitAll()
                 .requestMatchers("/api/**").authenticated()
                 .anyRequest().authenticated()
                 .and()
---
+++ src/main/java/com/example/events_app/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.example.events_app.controller;
+
+import com.example.events_app.dto.bonus.UserBonusHistoryResponseShortDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class TestController {
+    @GetMapping("/api/ping")
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("pong");
+    }
+}
